1fcd9f2045eb23eef3d35cc8eb19b0ace03dfbbe
HagOrMan <khagypi3@gmail.com>
2023-03-31 11:14:55 -0400

    Deleted unnecessary main class

--- pathfinder/src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-//import ca.mcmaster.cas.se2aa4.a3.island.InputFactories.InputParser;
-
-public class Main {
-    public static void main(String[] args) {
-//        InputParser inputParser = new InputParser();
-//        inputParser.createMesh(args);
-    }
-}
