9a56745b0fed9e0b04c082bb20ab9f65ca36744c
HagOrMan <khagypi3@gmail.com>
2023-04-05 16:21:59 -0400

    Set up getters and constructors for Nodes and Edges

--- pathfinder/src/main/java/ca/mcmaster/cas/se2aa4/a4/pathfinder/Edge.java
+++ pathfinder/src/main/java/ca/mcmaster/cas/se2aa4/a4/pathfinder/Edge.java
@@ -1,4 +1,38 @@
 package ca.mcmaster.cas.se2aa4.a4.pathfinder;
 
 public class Edge {
+
+    private final Node n1, n2;
+    private final int weight;
+
+
+    public Edge(Node n1, Node n2, int weight){
+        this.n1 = n1;
+        this.n2 = n2;
+        this.weight = weight;
+    }
+
+    public int getWeight(){
+        return weight;
+    }
+
+    public boolean equals(Edge other) {
+        return super.equals(other) || (n2.equals(other.getN1Idx()) && n1.equals(other.getN2Idx()));
+    }
+
+    public boolean equals(int n1Idx, int n2Idx) {
+        return (n1.equals(n1Idx) && n1.equals(n2Idx)) || (n2.equals(n1Idx) && n1.equals(n2Idx));
+    }
+
+    public boolean n1Equals(int index){
+        return this.n1.equals(index);
+    }
+
+
+    public int getN1Idx() {
+        return n1.getIndex();
+    }
+    public int getN2Idx() {
+        return n2.getIndex();
+    }
 }
--- pathfinder/src/main/java/ca/mcmaster/cas/se2aa4/a4/pathfinder/Node.java
+++ pathfinder/src/main/java/ca/mcmaster/cas/se2aa4/a4/pathfinder/Node.java
@@ -1,4 +1,31 @@
 package ca.mcmaster.cas.se2aa4.a4.pathfinder;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Node {
+
+    private final int index;
+    private static List<Integer> usedIndices = new ArrayList<>();
+
+    public Node(int index){
+        // Checks if the index is already used by another node, if so, sets new index to 1 higher than current highest index.
+        if (!usedIndices.contains(index)){
+            this.index = index;
+        }
+        else{
+            this.index = Collections.max(usedIndices) + 1;
+        }
+        usedIndices.add(this.index);
+    }
+
+
+    public boolean equals(int index) {
+        return this.index == index;
+    }
+
+    public int getIndex() {
+        return index;
+    }
 }
