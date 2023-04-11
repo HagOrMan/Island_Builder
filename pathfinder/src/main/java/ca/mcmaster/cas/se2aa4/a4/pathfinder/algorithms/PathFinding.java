b06fcdacc0c741d6a2fc429bc43fd4a64979cf02
HagOrMan <khagypi3@gmail.com>
2023-04-11 15:34:53 -0400

    F05 - interface lets one find a path from any start node in a graph

--- pathfinder/src/main/java/ca/mcmaster/cas/se2aa4/a4/pathfinder/algorithms/PathFinding.java
+++ pathfinder/src/main/java/ca/mcmaster/cas/se2aa4/a4/pathfinder/algorithms/PathFinding.java
@@ -1,4 +1,12 @@
 package ca.mcmaster.cas.se2aa4.a4.pathfinder.algorithms;
 
+import ca.mcmaster.cas.se2aa4.a4.pathfinder.Graph;
+import ca.mcmaster.cas.se2aa4.a4.pathfinder.Node;
+
+import java.util.Map;
+
 public interface PathFinding {
+
+    public Map<Node, Node> findPath(Graph graph, Node start);
+
 }
--- pathfinder/src/main/java/ca/mcmaster/cas/se2aa4/a4/pathfinder/algorithms/ShortestPath.java
+++ pathfinder/src/main/java/ca/mcmaster/cas/se2aa4/a4/pathfinder/algorithms/ShortestPath.java
@@ -1,4 +1,13 @@
 package ca.mcmaster.cas.se2aa4.a4.pathfinder.algorithms;
 
+import ca.mcmaster.cas.se2aa4.a4.pathfinder.Graph;
+import ca.mcmaster.cas.se2aa4.a4.pathfinder.Node;
+
+import java.util.Map;
+
 public class ShortestPath implements PathFinding{
+    @Override
+    public Map<Node, Node> findPath(Graph graph, Node start) {
+        return null;
+    }
 }
