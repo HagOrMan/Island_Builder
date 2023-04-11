886f36d3a1e84dc58ab466dbf0c7210c61853443
HagOrMan <khagypi3@gmail.com>
2023-04-11 13:24:56 -0400

    F03 - Added basic information like nodes to graph adt

--- pathfinder/src/main/java/ca/mcmaster/cas/se2aa4/a4/pathfinder/Graph.java
+++ pathfinder/src/main/java/ca/mcmaster/cas/se2aa4/a4/pathfinder/Graph.java
@@ -1,4 +1,26 @@
 package ca.mcmaster.cas.se2aa4.a4.pathfinder;
 
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
+
 public class Graph {
+
+    private List<Node> nodes;
+    private List<Edge> edges;
+    private Map<Edge, Integer> weights;
+
+    public Graph(){
+        nodes = new ArrayList<>();
+        edges = new ArrayList<>();
+        weights = new Hashtable<>();
+    }
+
+    // add edge
+    // add node
+    // get edge weight
+    // get nodes from edge
+    //
+
 }
