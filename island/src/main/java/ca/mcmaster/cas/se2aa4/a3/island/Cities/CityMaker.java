46e727697c266b3b19f77e5b934fc95f30af893f
HagOrMan <khagypi3@gmail.com>
2023-04-15 12:50:17 -0400

    F07 - city maker currently only returns large cities

--- island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/Cities/CityMaker.java
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/Cities/CityMaker.java
@@ -1,5 +1,11 @@
 package ca.mcmaster.cas.se2aa4.a3.island.Cities;
 
-public interface CityMaker {
-    CityOption getCityType(int population);
+
+public class CityMaker {
+
+    // Takes in a population and returns city type that should be used.
+    public CityOption getCityType(int population) {
+        return CityOption.CITY;
+    }
+
 }
