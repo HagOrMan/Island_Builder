78f4fd6c2a98341d6b039e7e75315ece58b60da8
HagOrMan <khagypi3@gmail.com>
2023-04-15 16:03:53 -0400

    F08 - user can specify number of cities with command line

--- island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/InputFactories/InputParser.java
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/InputFactories/InputParser.java
@@ -85,6 +85,11 @@ public class InputParser {
                 .hasArg()
                 .desc("Enter the Whittaker biome you want used")
                 .build();
+        Option cities = Option.builder("city")
+                .argName("num cities")
+                .hasArg()
+                .desc("Enter the number of cities you want to see (default = 0)")
+                .build();
 
         // add all options
         options.addOption(help);
@@ -99,6 +104,7 @@ public class InputParser {
         options.addOption(river);
         options.addOption(soil);
         options.addOption(biome);
+        options.addOption(cities);
 
         return options;
     }
@@ -111,12 +117,13 @@ public class InputParser {
     private void checkOptions(CommandLine line, Options options)  throws IOException {
 
         String inputFile = null, outputFile = null, mode = "default", heatmap = null, elevation = null, stringSeed = null;
-        String aquifer = null, soil = null, lake = null, river = null, biome = null;
+        String aquifer = null, soil = null, lake = null, river = null, biome = null, cities = null;
         long seed = -1;
 
         int numAquifers = 0;
         int numLakes = 0;
         int numRivers = 0;
+        int numCities = 0;
         
         // If they ask for help, displays options, and exits without generating a mesh.
         if (line.hasOption("h")){
@@ -180,6 +187,15 @@ public class InputParser {
         if (line.hasOption("biome")){
             biome = line.getOptionValue("biome");
         }
+        if (line.hasOption("city")){
+            cities = line.getOptionValue("city");
+            try {
+                numCities = Integer.parseInt(cities);
+            }
+            catch (NumberFormatException e) {
+                System.out.println("Invalid format for the number of cities, map now has 0 cities");
+            }
+        }
 
         // Ensures we have an input and output file before creating island.
         if (!(inputFile == null || outputFile == null)){
@@ -193,7 +209,7 @@ public class InputParser {
                 aMesh = handler.makeMesh(aMesh, heatmap, elevation, seed, numAquifers, soil, numLakes, numRivers);
             }
             else{
-                aMesh = handler.makeMesh(aMesh, elevation, seed, numAquifers, soil, numLakes, numRivers, biome);
+                aMesh = handler.makeMesh(aMesh, elevation, seed, numAquifers, soil, numLakes, numRivers, biome, numCities);
             }
             factory.write(aMesh, outputFile);
             
